cfb312219d41f9919aa3bd391897041460735b91
raynau_a <raynau_a@epitech.eu>
2014-02-03 23:09:34 +0100

    changé le script pour le rendre plus facile d'emploi

--- copy.sh
+++ copy.sh
@@ -1,8 +1,14 @@
-cp /TON_DOSSIER/Makefile Makefile
-cp /TON_DOSSIER/my.h my.h
-cp /TON_DOSSIER/libmy.a libmy.a
-cp /TON_DOSSIER/header.h header.h
-cp /TON_DOSSIER/get_next.c get_next.c
-cp /TON_DOSSIER/get_next.h get_next.h
-cp /TON_DOSSIER/main.c main.c
-cp -r /TON_DOSSIER/my_lib my_lib
+P=TON_DOSSIER
+
+## remplacer TON_DOSSIER par le chemin sur le script
+## faire ensuite un alias sur le script pour qu'il copie tout
+##    sur votre dossier courant
+
+cp /$P/Makefile Makefile
+cp /$P/my.h my.h
+cp /$P/libmy.a libmy.a
+cp /$P/header.h header.h
+cp /$P/get_next.c get_next.c
+cp /$P/get_next.h get_next.h
+cp /$P/main.c main.c
+cp -r /$P/my_lib my_lib
