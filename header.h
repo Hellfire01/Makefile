eff37c54b38b11b578b3eaca285002d03761e0e8
raynau_a <raynau_a@epitech.eu>
2014-02-03 15:42:40 +0100

    changed a bit the header

--- header.h
+++ header.h
@@ -5,11 +5,12 @@
 ** Login   <raynau_a@epitech.net>
 ** 
 ** Started on  Thu Jan  2 20:33:52 2014 a
-** Last update Thu Jan  2 20:34:08 2014 a
+** Last update Mon Feb  3 15:42:18 2014 a
 */
 
 #ifndef HEADER_H_
 # define HEADER_H_
 
+/* main.c */
 
 #endif /* !HEADER_H_ */
