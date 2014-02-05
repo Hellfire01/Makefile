51f6ca89990e2841a4ea550f5221709d4e9c5ccf
raynau_a <raynau_a@epitech.eu>
2014-02-05 18:15:41 +0100

    corrigé le script

--- copy.sh
+++ copy.sh
@@ -1,14 +1,13 @@
-P=TON_DOSSIER
+P=/home/raynau_a/rendu/GETNEXT-MAKEFILE-LIB
 
-## remplacer TON_DOSSIER par le chemin sur le script
+## remplacer le contenu de $P par le chemin sur le script
+## ( à l'endroit ou vous avez mit le script )
 ## faire ensuite un alias sur le script pour qu'il copie tout
 ##    sur votre dossier courant
 
-cp /$P/Makefile Makefile
-cp /$P/my.h my.h
-cp /$P/libmy.a libmy.a
-cp /$P/header.h header.h
-cp /$P/get_next.c get_next.c
-cp /$P/get_next.h get_next.h
-cp /$P/main.c main.c
+cp $P/Makefile Makefile
+cp $P/my.h my.h
+cp $P/libmy.a libmy.a
+cp $P/header.h header.h
+cp $P/main.c main.c
 cp -r /$P/my_lib my_lib
