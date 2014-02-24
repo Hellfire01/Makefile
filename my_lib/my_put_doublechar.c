f2cf0442ca38763875e084b9c4f17423d295eb1c
Hellfire01 <matthieu.raynaud-de-fitte@epitech.eu>
2014-02-24 22:28:29 +0100

    brioc77

--- my_lib/my_put_doublechar.c
+++ my_lib/my_put_doublechar.c
@@ -5,7 +5,7 @@
 ** Login   <raynau_a@epitech.net>
 ** 
 ** Started on  Fri Jan 24 02:29:50 2014 a
-** Last update Mon Feb 10 16:17:37 2014 a
+** Last update Mon Feb 24 22:28:06 2014 matthieu raynaud de fitte
 */
 
 #include <stdlib.h>
@@ -13,6 +13,7 @@
 
 /*
 ** la fonction my_put_doublechar affiche tout le contenu d'un tableau de char.
+** ATTENTION ! ==> il n'affiche pas les lignes vides même s'il y en a
 */
 
 int     my_put_doublechar(char **str)
@@ -22,8 +23,11 @@ int     my_put_doublechar(char **str)
   a = 0;
   while (str[a] != NULL)
     {
-      my_putstr(str[a]);
-      my_putchar('\n');
+      if (str[a][0] != '\0')
+	{
+	  my_putstr(str[a]);
+	  my_putchar('\n');
+	}
       a = a + 1;
     }
   return (0);
